d168cec04104a98bf5da77c620138cd7d81c4bea
colefromportland <colefromportland@gmail.com>
2019-05-16 12:30:20 -0600

    begin building external helper && mathmatic methods

--- calculator_app/app/src/main/java/com/projects/debugstatement/calculator_app/ExtActivity.java
+++ calculator_app/app/src/main/java/com/projects/debugstatement/calculator_app/ExtActivity.java
@@ -3,12 +3,20 @@ package com.projects.debugstatement.calculator_app;
 // helper method && operations class
 public class ExtActivity {
 
+    // define global vars
+    String operator;
+    int operand1, operand2;
+    
     // build parse calc function
         // parse to correct math method based on str operator
         // split numerical vals at operator
             // store each numerical val in global var
             // store operator in var
-    // public parseOperator
+    public void parseOperator(String input)
+    {
+
+        input.split()
+    }
 
     // public float add
 
