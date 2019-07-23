4c3d80291b1e7f6d063b1b92642fac23db0618f8
colefromportland <colefromportland@gmail.com>
2019-07-23 11:27:30 -0600

    building parseOperand() / parseOperator() / setOperator() methods

--- calculator_app/app/src/main/java/com/projects/debugstatement/calculator_app/ExtActivity.java
+++ calculator_app/app/src/main/java/com/projects/debugstatement/calculator_app/ExtActivity.java
@@ -1,5 +1,8 @@
 package com.projects.debugstatement.calculator_app;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 // helper method && operations class
 public class ExtActivity {
 
@@ -9,57 +12,73 @@ public class ExtActivity {
     // store each numerical val in global var
     // store operator in var
 
-    // possible operators to be eval'd
-    String [] operators = {"+", "-", "*", "/"};
-
     // define global vars
-    String operator;
-    float operand1, operand2;
+    float operand1 = 0.0f, operand2 = 0.0f;
+    float res;
+    boolean _add, _sub, _multi, _div;
 
-    // indentify
-    public void parseOperator(String input)
+
+    // set global math operation
+    public void setOperator(String optr)
     {
-        for (String o : operators) {
-            if (input.contains(o)) {
-                String [] nums = input.split(o);
-                for(String n : nums) {
-                    System.out.println(n);
-                }
-            }
-        }
-
-        // identify specific math func
-        switch (operator) {
-            case "+": doAdd(operand1, operand2);
+        switch(optr) {
+            case "+": _add = true; _sub = _div = _multi = false;
                 break;
-            case "-": doSub(operand1, operand2);
+            case "-": _sub = true; _add = _multi = _div = false;
                 break;
-            case "*": doMulti(operand1, operand2);
+            case "x": _multi = true; _add = _sub =  _div = false;
                 break;
-            case "/": doDiv(operand1, operand2);
-                break;
-            default: doAdd(operand1, operand2);
+            case "/": _div = true;_add = _sub = _multi = false;
                 break;
         }
     }
 
-    public float doAdd(float op1, float op2) {
-        float res = (op1 + op2);
+    // parse to correct mathematical eval method
+    public float parseOperator(String input)
+    {
+        // identify specific math func
+        if (_add) {
+            parseOperands(input);
+            res = doAdd(operand1, operand2);
+        }
+
         return res;
+    }
+
+    // parse operands from input string
+    public void parseOperands(String oprnd)
+    {
+        String op;
+        String [] oprnds;
+
+        if (_add) {
+            oprnds = oprnd.split("+");
+
+            for(String o:oprnds) {
+                if(operand1 == 0.0f) { operand1 = Float.parseFloat(o); }
+                else { operand2 = Float.parseFloat(o); }
+            }
+        }
+    }
+
+    // mathematical functions
+    public float doAdd(float op1, float op2) {
+        float sum = (op1 + op2);
+        return sum;
     }
 
     public float doSub(float op1, float op2) {
-        float res = (op1 - op2);
-        return res;
+        float sum = (op1 - op2);
+        return sum;
     }
 
     public float doMulti(float op1, float op2) {
-        float res = (op1 * op2);
-        return res;
+        float sum = (op1 * op2);
+        return sum;
     }
 
     public float doDiv(float op1, float op2) {
-        float res = (op1 / op2);
-        return res;
+        float sum = (op1 / op2);
+        return sum;
     }
 }
